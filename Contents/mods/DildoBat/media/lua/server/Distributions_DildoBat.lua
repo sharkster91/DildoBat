6538d1603219b8b3587b4b6225e3dcd67cf0b666
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-10-13 14:21:26 -0400

    Initial Commit

---
+++ Contents/mods/DildoBat/media/lua/server/Distributions_DildoBat.lua
@@ -0,0 +1,12 @@
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions["list"]["GigamartToys"].items, "DB.DildoBat");
+table.insert(ProceduralDistributions["list"]["GigamartToys"].items, 2);
+table.insert(ProceduralDistributions["list"]["CrateToys"].items, "DB.DildoBat");
+table.insert(ProceduralDistributions["list"]["CrateToys"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["GigamartToys"].items, "DB.DildoBat");
+table.insert(ProceduralDistributions["list"]["GigamartToys"].items, 2);
+table.insert(ProceduralDistributions["list"]["CrateToys"].items, "DB.DildoBat");
+table.insert(ProceduralDistributions["list"]["CrateToys"].items, 2);
+
---
+++ Contents/mods/TwitchBat/media/lua/server/Distributions_TwitchBat.lua
@@ -0,0 +1,12 @@
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions["list"]["GigamartToys"].items, "TB.TwitchBat");
+table.insert(ProceduralDistributions["list"]["GigamartToys"].items, 2);
+table.insert(ProceduralDistributions["list"]["CrateToys"].items, "TB.TwitchBat");
+table.insert(ProceduralDistributions["list"]["CrateToys"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["GigamartToys"].items, "TB.TwitchBat");
+table.insert(ProceduralDistributions["list"]["GigamartToys"].items, 2);
+table.insert(ProceduralDistributions["list"]["CrateToys"].items, "TB.TwitchBat");
+table.insert(ProceduralDistributions["list"]["CrateToys"].items, 2);
+
