5544d687a98e0219af03e1fd87a5c79d008cbc6f
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-11-05 14:06:16 -0400

    Update and fix

--- Contents/mods/TwitchBat/media/lua/server/Distributions_TwitchBat.lua
+++ Contents/mods/TwitchBat/media/lua/server/Distributions_TwitchBat.lua
@@ -1,12 +1,22 @@
 require 'Items/ProceduralDistributions'
 
-table.insert(ProceduralDistributions["list"]["GigamartToys"].items, "TB.TwitchBat");
-table.insert(ProceduralDistributions["list"]["GigamartToys"].items, 2);
-table.insert(ProceduralDistributions["list"]["CrateToys"].items, "TB.TwitchBat");
-table.insert(ProceduralDistributions["list"]["CrateToys"].items, 2);
+table.insert(ProceduralDistributions.list.GigamartToys.items, "DB.TwitchBat");
+table.insert(ProceduralDistributions.list.GigamartToys.items, 2);
 
-table.insert(ProceduralDistributions["list"]["GigamartToys"].items, "TB.TwitchBat");
-table.insert(ProceduralDistributions["list"]["GigamartToys"].items, 2);
-table.insert(ProceduralDistributions["list"]["CrateToys"].items, "TB.TwitchBat");
-table.insert(ProceduralDistributions["list"]["CrateToys"].items, 2);
+table.insert(ProceduralDistributions.list.CrateToys.items, "DB.TwitchBat");
+table.insert(ProceduralDistributions.list.CrateToys.items, 2);
 
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, "DB.TwitchBat");
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.TwitchBat");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "DB.TwitchBat");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.TwitchBat");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+
+table.insert(ProceduralDistributions.list.BathroomCounter.items, "DB.TwitchBat");
+table.insert(ProceduralDistributions.list.BathroomCounter.items, 2);
