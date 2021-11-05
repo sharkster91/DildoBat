866954ccdd7880a3e098e1860f5bca3ad5326211
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-11-05 13:10:49 -0400

    Update Distributions_DildoBat.lua

--- Contents/mods/DildoBat/media/lua/server/Distributions_DildoBat.lua
+++ Contents/mods/DildoBat/media/lua/server/Distributions_DildoBat.lua
@@ -1,12 +1,22 @@
 require 'Items/ProceduralDistributions'
 
-table.insert(ProceduralDistributions["list"]["GigamartToys"].items, "DB.DildoBat");
-table.insert(ProceduralDistributions["list"]["GigamartToys"].items, 2);
-table.insert(ProceduralDistributions["list"]["CrateToys"].items, "DB.DildoBat");
-table.insert(ProceduralDistributions["list"]["CrateToys"].items, 2);
+table.insert(ProceduralDistributions.list.GigamartToys.items, "DB.DildoBat");
+table.insert(ProceduralDistributions.list.GigamartToys.items, 2);
 
-table.insert(ProceduralDistributions["list"]["GigamartToys"].items, "DB.DildoBat");
-table.insert(ProceduralDistributions["list"]["GigamartToys"].items, 2);
-table.insert(ProceduralDistributions["list"]["CrateToys"].items, "DB.DildoBat");
-table.insert(ProceduralDistributions["list"]["CrateToys"].items, 2);
+table.insert(ProceduralDistributions.list.CrateToys.items, "DB.DildoBat");
+table.insert(ProceduralDistributions.list.CrateToys.items, 2);
 
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, "DB.DildoBat");
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.DildoBat");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "DB.DildoBat");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.DildoBat");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+
+table.insert(ProceduralDistributions.list.BathroomCounter.items, "DB.DildoBat");
+table.insert(ProceduralDistributions.list.BathroomCounter.items, 2);
