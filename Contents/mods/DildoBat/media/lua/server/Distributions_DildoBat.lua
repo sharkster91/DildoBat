391b6d2df80cad2e92fc5d4f1fc7fdd3dc7d846d
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-05 11:40:24 -0500

    Added Louisville Plugger

--- Contents/mods/DildoBat/media/lua/server/Distributions_DildoBat.lua
+++ Contents/mods/DildoBat/media/lua/server/Distributions_DildoBat.lua
@@ -20,3 +20,24 @@ table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
 
 table.insert(ProceduralDistributions.list.BathroomCounter.items, "DB.DildoBat");
 table.insert(ProceduralDistributions.list.BathroomCounter.items, 2);
+
+table.insert(ProceduralDistributions.list.GigamartToys.items, "DB.LouisvillePlugger");
+table.insert(ProceduralDistributions.list.GigamartToys.items, 2);
+
+table.insert(ProceduralDistributions.list.CrateToys.items, "DB.LouisvillePlugger");
+table.insert(ProceduralDistributions.list.CrateToys.items, 2);
+
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, "DB.LouisvillePlugger");
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.LouisvillePlugger");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeMan.items, "DB.LouisvillePlugger");
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 2);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.LouisvillePlugger");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+
+table.insert(ProceduralDistributions.list.BathroomCounter.items, "DB.LouisvillePlugger");
+table.insert(ProceduralDistributions.list.BathroomCounter.items, 2);
