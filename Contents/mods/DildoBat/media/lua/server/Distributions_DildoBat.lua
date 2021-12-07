f6740f06438422d1facf2a4d052e1c67e9ee686d
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-07 03:56:09 -0500

    Update Distributions_DildoBat.lua

--- Contents/mods/DildoBat/media/lua/server/Distributions_DildoBat.lua
+++ Contents/mods/DildoBat/media/lua/server/Distributions_DildoBat.lua
@@ -1,38 +1,37 @@
 require 'Items/ProceduralDistributions'
 
-
 table.insert(ProceduralDistributions.list.StripClubDressers.items, "DB.DildoBat");
-table.insert(ProceduralDistributions.list.StripClubDressers.items, 2);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 3);
 
 table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, "DB.DildoBat");
-table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, 2);
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, 1);
 
 table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.DildoBat");
-table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 1);
 
 table.insert(ProceduralDistributions.list.WardrobeMan.items, "DB.DildoBat");
-table.insert(ProceduralDistributions.list.WardrobeMan.items, 2);
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 1);
 
 table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.DildoBat");
-table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 1);
 
 table.insert(ProceduralDistributions.list.BathroomCounter.items, "DB.DildoBat");
-table.insert(ProceduralDistributions.list.BathroomCounter.items, 2);
+table.insert(ProceduralDistributions.list.BathroomCounter.items, 1);
 
 table.insert(ProceduralDistributions.list.StripClubDressers.items, "DB.LouisvillePlugger");
-table.insert(ProceduralDistributions.list.StripClubDressers.items, 2);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 3);
 
-table.insert(ProceduralDistributions.list.BathroomCounter.items, "DB.LouisvillePlugger");
-table.insert(ProceduralDistributions.list.BathroomCounter.items, 2);
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, "DB.LouisvillePlugger");
+table.insert(ProceduralDistributions.list.BathroomCounterNoMeds.items, 1);
 
 table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.LouisvillePlugger");
-table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 1);
 
 table.insert(ProceduralDistributions.list.WardrobeMan.items, "DB.LouisvillePlugger");
-table.insert(ProceduralDistributions.list.WardrobeMan.items, 2);
+table.insert(ProceduralDistributions.list.WardrobeMan.items, 1);
 
 table.insert(ProceduralDistributions.list.WardrobeWoman.items, "DB.LouisvillePlugger");
-table.insert(ProceduralDistributions.list.WardrobeWoman.items, 2);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 1);
 
 table.insert(ProceduralDistributions.list.BathroomCounter.items, "DB.LouisvillePlugger");
-table.insert(ProceduralDistributions.list.BathroomCounter.items, 2);
+table.insert(ProceduralDistributions.list.BathroomCounter.items, 1);
